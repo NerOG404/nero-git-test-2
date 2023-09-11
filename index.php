aad17eb4cdd586eec23f2de2eb356bcb4287a8d9
NerOG404 <mohamadsaefudin042@gmail.com>
2023-09-11 15:15:57 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat eius labore dignissimos quo assumenda, nulla, fugiat voluptate excepturi ab reprehenderit dolorum praesentium maxime tenetur neque eligendi accusamus quis optio voluptas?</p>
 </body>
 </html>
